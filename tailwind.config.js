5ae11d496daa1848e30d173d6416dd8002422f74
Imkn1018 <o2oneru2505@gmail.com>
2021-04-24 07:43:58 +0900

    change tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js'],
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
